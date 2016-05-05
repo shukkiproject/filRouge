f102024a583b4da40167f69741ad20bd113d9cec
imie <imie@localhost.localdomain>
2016-05-05 19:09:42 +0200

    add of messages and series user

--- src/SiteBundle/Controller/UserController.php
+++ src/SiteBundle/Controller/UserController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use SiteBundle\Entity\User;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\MessageBundle\Controller\MessageController;
 
 
 /**
@@ -40,22 +41,24 @@ class UserController extends Controller
      * @Route("/profil", name="user_profil")
      * @Method("GET")
      */
-    // public function profilAction()
-    // {
-    //     $user = $this->getUser();
-    //     $em = $this->getDoctrine()->getManager();
-    //     $comments = $em->getRepository('SiteBundle:Comment')->findByUserid($user->getId());
-    //     $series = $em->getRepository('SiteBundle:Series')->findByUserid($user->getId());
-    //     if (!is_object($user) || !$user instanceof UserInterface) {
-    //         throw new AccessDeniedException('This user does not have access to this section.');
-    //     }
-
-    //     return $this->render('FOSUserBundle:Profile:show.html.twig', array(
-    //         'user' => $user,
-    //         'comments' => $comment,
-    //         'series' => $series,
-    //     ));
-    // }
+    public function profilAction()
+    {
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        //$comments = $em->getRepository('SiteBundle:Comment')->findByUserid($user->getId());
+        // $series = $em->getRepository('SiteBundle:Series')->findByUserid($user->getId());
+        // if (!is_object($user) || !$user instanceof UserInterface) {
+        //     throw new AccessDeniedException('This user does not have access to this section.');
+        // }
+        $provider = $this->container->get('fos_message.provider');
+        $threads = $provider->getInboxThreads();
+        return $this->render('users/showProfil.html.twig', array(
+            'user' => $user,
+            'threads' => $threads,
+            //'comments' => $comments,
+            // 'series' => $series,
+        ));
+    }
 
      /**
      * Finds and displays a User.
@@ -63,17 +66,20 @@ class UserController extends Controller
      * @Route("/{id}", name="user_show")
      * @Method("GET")
      */
-    // public function showAction($id)
-    // {   
-    //     $em = $this->getDoctrine()->getManager();
-    //     $user = $em->getRepository('SiteBundle:User')->findOneById($id);
-    //     $comments = $em->getRepository('SiteBundle:Comment')->findByUserid($id);
-    //     $series = $em->getRepository('SiteBundle:Series')->findByUserid($id);
-    //     return $this->render('users/show.html.twig', array(
-    //         'user' => $user,
-    //         'comments' => $comment,
-    //         'series' => $series,
-    //     ));
-    // }
+    public function showAction($id)
+    {   
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('SiteBundle:User')->findOneById($id);
+        $provider = $this->container->get('fos_message.provider');
+        $threads = $provider->getInboxThreads();
+        $comments = $em->getRepository('SiteBundle:Comment')->findByUserid($id);
+        $series = $em->getRepository('SiteBundle:Series')->findByUserid($id);
+        return $this->render('users/showUser.html.twig', array(
+            'user' => $user,
+            'comments' => $comments,
+            'series' => $series,
+        ));
+    }
 
 }
